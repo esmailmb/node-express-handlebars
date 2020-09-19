e5fac42230bde289969f229f40a4ad3c747552a6
esmail mahbub <esml.mahbub>
2020-09-19 19:56:49 -0400

    update files

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'password',
     database: 'burgers_db'
