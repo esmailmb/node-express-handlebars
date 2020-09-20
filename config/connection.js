82d7addff5c6c4d6d69f2952b3dc8e9642386741
Esmail Mahbub <64238744+esmailmb@users.noreply.github.com>
2020-09-19 21:08:45 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'burgers_db'
